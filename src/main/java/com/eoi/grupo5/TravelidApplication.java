3868bdd30e858fa80c67db6604aab9f73a84ebc6
Juanmi <jmcostalucena2000@gmail.com>
2024-06-16 17:37:00 +0200

    Proyecto Entero

---
+++ src/main/java/com/eoi/grupo5/TravelidApplication.java
@@ -0,0 +1,13 @@
+package com.eoi.grupo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelidApplication.class, args);
+	}
+
+}
