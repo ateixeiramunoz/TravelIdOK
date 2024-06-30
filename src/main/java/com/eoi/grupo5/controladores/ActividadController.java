54fe300e4b29890f51beaf8c220af642725439d6
Juanmi <jmcostalucena2000@gmail.com>
2024-06-30 13:52:36 +0200

    Arreglado diseño cards en plantilla hoteles. Ligera mejora en textos de miembros de equipo en plantilla about

--- src/main/java/com/eoi/grupo5/controladores/ActividadController.java
+++ src/main/java/com/eoi/grupo5/controladores/ActividadController.java
@@ -21,7 +21,6 @@ public class ActividadController {
     public String listaActividades(Model modelo) {
         List<Actividad> actividades = servicio.buscarEntidades();
         modelo.addAttribute("lista", actividades);
-
         return "actividades";
     }
 
